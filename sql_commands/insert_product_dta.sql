d035271b1a9c9cdf2d63965deca5bc1635dcd37b
JKaypa <jose_kaypa@hotmail.com>
2025-01-29 15:01:12 -0500

    insert product data

---
+++ sql_commands/insert_product_dta.sql
@@ -0,0 +1,16 @@
+USE juice_sales;
+
+INSERT INTO tb_product (
+	code, description, flavor, size, container, list_price
+) VALUES (
+	"1040107", "Light", "Watermelon", "350ml", "can", 4.56
+);
+
+INSERT INTO tb_product VALUES (
+	"1040108", "Light", "Guanabana", "350ml", "can", 4.00
+);
+
+INSERT INTO tb_product VALUES
+("1040109", "Light", "asai", "350ml", "can", 5.60), 
+("1040110", "Light", "Mango", "350ml", "can", 3.50),
+("1040111", "Light", "Apple", "350ml", "can", 6.00);

7212fc4c6edb367979adc86b0b57e28ad88c363d
JKaypa <jose_kaypa@hotmail.com>
2025-01-29 15:02:24 -0500

    insert customer data

---
+++ sql_commands/insert_customer_data.sql
@@ -0,0 +1,8 @@
+USE juice_sales;
+
+INSERT INTO tb_customer (
+	dni, name, address, neighborhood, city, state, zip_code, date_of_birth, age, gender, credit_limit, purchase_vol, first_purchase
+) VALUES 
+("9283760794", "Edson Calisaya", "Sta Úrsula Xitla", "Barrio del Niño Jesús", "Ciudad de México", "EM", "22002002", "1995-01-07", 25, "M", 70000, 160000, 1),
+("7771579779", "Marcelo Perez", "F.C. de Cuernavaca 13", "Carola", "Ciudad de México", "EM", "88202912", "1992-01-25", 29, "M", 70000, 160000, 1),
+("5576228758", "Patricia Olivera", "Pachuca 75", "Condesa", "Ciudad de México", "EM", "88192029", "1995-01-14", 25, "F", 70000, 160000, 1);

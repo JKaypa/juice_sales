ec6392dad3569797d915d7eb0d324b23d49486de
JKaypa <jose_kaypa@hotmail.com>
2025-01-30 18:18:49 -0500

    update row

--- sql_commands/update_tb_customer.sql
+++ sql_commands/update_tb_customer.sql
@@ -1,3 +1,7 @@
 SELECT * FROM juice_sales.tb_customer;
 
 UPDATE tb_customer SET purchase_vol = purchase_vol / 10;
+
+SELECT * FROM tb_customer WHERE dni = "5840119709";
+
+UPDATE tb_customer SET address = "Jorge Emilio 23", neighborhood = "San Antonio", city = "Guadalajara", state = "JC", zip_code = "44700000" WHERE dni = "5840119709";

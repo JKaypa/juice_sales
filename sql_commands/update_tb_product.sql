b241356af123d847e0c49c1e5193bf43de4fabf7
JKaypa <jose_kaypa@hotmail.com>
2025-01-30 18:00:30 -0500

    update more fields

--- sql_commands/update_tb_product.sql
+++ sql_commands/update_tb_product.sql
@@ -1,3 +1,5 @@
 SELECT * FROM juice_sales.tb_product;
 
 UPDATE tb_product SET list_price = 5 WHERE code = "1000889";
+
+UPDATE tb_product SET description = "Sabor de la Montaña", size = "1 Litro", container = "Botella PET" WHERE code = "1000889";

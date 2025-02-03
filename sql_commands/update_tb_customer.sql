f5cac3ea6af14c578435ec4fa3b84b5dde5c4a82
JKaypa <jose_kaypa@hotmail.com>
2025-02-03 16:57:47 -0500

    format

--- sql_commands/update_tb_customer.sql
+++ sql_commands/update_tb_customer.sql
@@ -4,4 +4,10 @@ UPDATE tb_customer SET purchase_vol = purchase_vol / 10;
 
 SELECT * FROM tb_customer WHERE dni = "5840119709";
 
-UPDATE tb_customer SET address = "Jorge Emilio 23", neighborhood = "San Antonio", city = "Guadalajara", state = "JC", zip_code = "44700000" WHERE dni = "5840119709";
+UPDATE tb_customer 
+SET address = "Jorge Emilio 23", 
+	neighborhood = "San Antonio", 
+	city = "Guadalajara", 
+    state = "JC", 
+    zip_code = "44700000" 
+WHERE dni = "5840119709";

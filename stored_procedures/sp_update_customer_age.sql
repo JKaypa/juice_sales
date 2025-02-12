4e7e5968be63529b9d929ee11c1679e6e1bc5a1c
JKaypa <jose_kaypa@hotmail.com>
2025-02-12 07:07:11 -0500

    fix syntax

--- stored_procedures/sp_update_customer_age.sql
+++ stored_procedures/sp_update_customer_age.sql
@@ -1,4 +1,4 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `calculate_age`()
 BEGIN
-UPDATE tb_customer SET date_of_birth = (SELECT TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()));
+UPDATE tb_customer SET age = (SELECT TIMESTAMPDIFF(YEAR, date_of_birth, CURDATE()));
 END
